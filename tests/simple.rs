3cba8d10abf7cd7d8251ee5da31d87f95d345801
Frontier789 <fr0nt13r789@gmail.com>
2020-05-09 00:10:04 +0200

    Updated test

--- tests/simple.rs
+++ tests/simple.rs
@@ -1,86 +1,83 @@
 use std::fmt::Debug;
 use std::ops::Neg;
-use std::ops::Shr;
+use std::ops::Shl;
 
 trait Widget: Debug {}
 
 struct WidgetParser {}
 
 impl WidgetParser {
-    fn doprint() -> bool { false }
+    fn doprint() -> bool {
+        true
+    }
     fn open_widget<T>(w: &T)
-        where
-            T: Widget,
+    where
+        T: Widget,
     {
         // you can use thread_local here as self
-        if Self::doprint() { println!("A widget is opened"); }
+        if Self::doprint() {
+            println!("Widget {:?} is opened", w);
+        }
     }
-    fn close_widget<T>(_w: &T)
-        where
-            T: Widget,
-    {
-        if Self::doprint() { println!("A widget is closed"); }
+    fn close_widgets(n: u32) {
+        if Self::doprint() {
+            println!("< {} --", n);
+        }
     }
     fn enter_builder(cache_id: u64) {
-        if Self::doprint() { println!("Entered a new builder, id: {}", cache_id); };
+        if Self::doprint() {
+            println!("Entered a new builder, id: {}", cache_id);
+        };
     }
     fn leave_builder() {
-        if Self::doprint() { println!("Left the last builder"); }
+        if Self::doprint() {
+            println!("Left the last builder");
+        }
     }
     fn register_param<T>(param: &T)
-        where
-            T: std::fmt::Debug,
+    where
+        T: std::fmt::Debug,
     {
-        if Self::doprint() { println!("Registered param {:?}", param); };
+        if Self::doprint() {
+            println!("Registered param {:?}", param);
+        };
     }
 }
 
 #[derive(Debug)]
-struct WidgetAdder {}
+struct WidgetAdder {
+    depth: u32,
+}
 
 impl WidgetAdder {
     pub fn new() -> WidgetAdder {
-        // println!("-- children =>");
-        WidgetAdder {}
+        WidgetAdder { depth: 1 }
     }
 }
 
 impl Drop for WidgetAdder {
     fn drop(&mut self) {
-        println!("< children --")
+        if self.depth > 0 {
+            WidgetParser::close_widgets(self.depth);
+        }
     }
 }
 
-impl Shr<()> for WidgetAdder {
+impl Shl<()> for WidgetAdder {
     type Output = ();
 
-    fn shr(self, _rhs: ()) -> Self::Output {
+    fn shl(self, _rhs: ()) -> Self::Output {
         ()
     }
 }
 
-#[derive(Debug)]
-struct WidgetAdderLeaf {}
+impl Shl<WidgetAdder> for WidgetAdder {
+    type Output = WidgetAdder;
 
-impl Drop for WidgetAdderLeaf {
-    fn drop(&mut self) {
-        println!("< children --")
-    }
-}
-
-impl Shr<()> for WidgetAdderLeaf {
-    type Output = WidgetAdderLeaf;
-
-    fn shr(self, _rhs: ()) -> Self::Output {
+    fn shl(mut self, mut rhs: WidgetAdder) -> Self::Output {
+        rhs.depth = 0;
+        self.depth += 1;
         self
-    }
-}
-
-impl Shr<WidgetAdder> for WidgetAdder {
-    type Output = WidgetAdderLeaf;
-
-    fn shr(self, _rhs: WidgetAdder) -> Self::Output {
-        WidgetAdderLeaf{}
     }
 }
 
@@ -88,18 +85,18 @@ macro_rules! impl_widget {
     ($t:ty) => {
         impl Neg for $t {
             type Output = WidgetAdder;
-        
+
             fn neg(self) -> Self::Output {
-                println!("Constructed {:?}", self);
+                WidgetParser::open_widget(&self);
                 WidgetAdder::new()
             }
         }
-        
+
         impl Widget for $t {}
-    }
+    };
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct Layout {
     height: f32,
 }
@@ -112,8 +109,8 @@ struct Button {
 impl_widget!(Button);
 
 fn call<T>(mut f: T)
-    where
-        T: FnMut() -> (),
+where
+    T: FnMut() -> (),
 {
     f();
 }
@@ -127,24 +124,22 @@ trait UIBuilder {
     fn build(&self);
 }
 
+#[allow(unused_must_use)]
 impl UIBuilder for UIExperiment {
     #[glui_proc::cache]
     fn build(&self) {
-        call(|| {
+        call(move || {
             -Button {
                 text: "Imma button inna call".to_owned(),
             };
         });
 
-        -Layout {
-            height: 200.0,
-        } >> for i in 1..6 {
-            button_builder(format!("button text is {}", i));
-        };
+        -Layout { height: 200.0 }
+            << for i in 1..6 {
+                button_builder(format!("button text is {}", i));
+            };
 
-        -Layout {
-            height: 400.0,
-        } >> {
+        -Layout { height: 400.0 } << {
             -Button {
                 text: format!("A_{}", self.data),
             };
@@ -153,18 +148,15 @@ impl UIBuilder for UIExperiment {
             };
         };
 
-        -Layout {
-            height: 100.0,
-        } >> {
-            -Layout {
-                height: 200.0,
-            } >> -Button {
+        -Layout { height: 100.0 }
+            << -Layout { height: 200.0 }
+            << -Button {
                 text: format!("A_{}", self.data),
             };
-        };
     }
 }
 
+#[allow(unused_must_use)]
 #[glui_proc::cache]
 fn button_builder(text: String) {
     -Button { text };
